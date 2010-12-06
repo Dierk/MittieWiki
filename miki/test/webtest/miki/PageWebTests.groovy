d6cfe00519096fc004b72d085f14c653639b89c7
Dierk Koenig <dierk.koenig@canoo.com>
2010-12-06 08:33:29 +0100

    calendar events testcase added

--- miki/test/webtest/miki/PageWebTests.groovy
+++ miki/test/webtest/miki/PageWebTests.groovy
@@ -13,4 +13,17 @@ class PageWebTests extends grails.util.WebTest {
         verifyTitle 'SimplePage'
     }
 
+    void testEventsPage() {
+        File eventsFile = new File('test/pages/EventProvider.wiki')
+        if (eventsFile.exists()) eventsFile.delete()
+        eventsFile.withWriter('ISO-8859-1') { out ->
+            out << new Date().format('dd.MM.yy') << " first date"
+        }
+        invoke 'SimplePage'
+        clickLink 'events'
+        verifyTitle 'Events'
+        clickLink xpath:'//td[@class="today events"]//a[text()="EventProvider"]'
+        verifyTitle 'EventProvider'
+    }
+
 }
