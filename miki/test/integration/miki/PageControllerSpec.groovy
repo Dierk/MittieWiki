20a804a407a9b8570278653c192ffbd0de4df66c
Dierk Koenig <dierk.koenig@canoo.com>
2010-12-14 00:15:53 +0100

    bringing code coverage to 93%; the remaining 7% are cobertura counting errors

--- miki/test/integration/miki/PageControllerSpec.groovy
+++ miki/test/integration/miki/PageControllerSpec.groovy
@@ -1,18 +1,19 @@
 package miki
 
 import grails.plugin.spock.ControllerSpec
+import groovy.mock.interceptor.MockFor
 
 class PageControllerSpec extends ControllerSpec {
 
     def "index action dispatches to view based on availability of the page"() {
-        given:
+        given: 'a page controller'
         PageController controller = new PageController()
 
-        when:
+        when: "page $page is requested"
         controller.params.page = page
         controller.index()
 
-        then:
+        then: "we should delegate to the $view view"
         controller.modelAndView.viewName == view
         controller.modelAndView.model.linkedHashMap.name == page
 
@@ -20,6 +21,44 @@ class PageControllerSpec extends ControllerSpec {
         page            | view
         'SimplePage'    | 'show'
         'NoSuchPage'    | 'create'
+    }
+
+    def "open action calls finder"() {
+        given: 'a page controller'
+        PageController controller = new PageController()
+        def stringMock = new MockFor(String)
+        stringMock.demand.execute(1..1) {->
+            [waitForProcessOutput: {-> 0 }]
+        }
+
+        when: "open new page is requested"
+        stringMock.use {
+            controller.open()
+        }
+
+        then: "we should delegate to the index view"
+        controller.response.status == 200
+
+    }
+
+    def "edit action calls editor"() {
+        given: 'a page controller'
+        PageController controller = new PageController()
+        controller.cookService = new CookService()
+        def stringMock = new MockFor(String)
+        stringMock.demand.plus(1..1) { op -> op }
+        stringMock.demand.execute(1..1) {->
+            [waitForProcessOutput: {-> 0 }]
+        }
+
+        when: "open new page is requested"
+        controller.params.page = "SimplePage"
+        stringMock.use {
+            controller.edit()
+        }
+
+        then: "we should delegate to the index view"
+        controller.response.status == 200
     }
 
 }
