1489589e5fdb68181f16765de6c49011f6592c4d
Dierk Koenig <dierk.koenig@canoo.com>
2018-04-04 09:40:51 +0200

    remove hibernate from logging

--- miki/grails-app/conf/Config.groovy
+++ miki/grails-app/conf/Config.groovy
@@ -113,10 +113,7 @@ log4j.main = {
            'org.codehaus.groovy.grails.web.mapping',        // URL mapping
            'org.codehaus.groovy.grails.commons',            // core / classloading
            'org.codehaus.groovy.grails.plugins',            // plugins
-           'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
-           'org.springframework',
-           'org.hibernate',
-           'net.sf.ehcache.hibernate'
+           'org.springframework'
 }
 
 // set per-environment serverURL stem for creating absolute links
